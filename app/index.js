ced045491b08c87b759f93caee5bc8d5e1c6727d
lamhotsimamora <lamhotsimamora36@gmail.com>
2018-02-16 16:21:53 +0700

    + add modal by stefanus n p

--- app/index.js
+++ app/index.js
@@ -1,3 +1,28 @@
-/**
- * -----
- */
+Garuda('btn_modal_info').when('click',e=>{
+	Garuda('#garudamodal').show()
+	$('#modal_info').fadeIn(1000);
+});
+
+Garuda('btn_modal_warning').when('click',e=>{
+	Garuda('#garudamodal').show()
+	$('#modal_warning').fadeIn(1000);
+});
+
+Garuda('btn_modal_default').when('click',e=>{
+	Garuda('#garudamodal').show()
+	$('#modal_default').fadeIn(1000);
+});
+
+Garuda('btn_modal_success').when('click',e=>{
+	Garuda('#garudamodal').show()
+	$('#modal_success').fadeIn(1000);
+});
+
+Garuda('btn_modal_danger').when('click',e=>{
+	Garuda('#garudamodal').show()
+	$('#modal_danger').fadeIn(1000);
+});
+
+Garuda('#closeBtn').when('click',$=>{
+	Garuda('#garudamodal').hide()
+});
